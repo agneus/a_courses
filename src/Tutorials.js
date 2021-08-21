809c1f4b740817811b136b22e875ee6e312efe38
Agneus <76863188+agneus@users.noreply.github.com>
2021-08-20 19:35:44 -0500

    Add files via upload

--- src/Tutorials.js
+++ src/Tutorials.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import './Tutorials.css'
 
 function Tutorials() {
     return (
-        <div>
-            <div>
-                <h2>Random Tutorial</h2>
+        <div className='tutorials'>
+            <div className='bar2'>
+                <button className='bar_button'>Data Handling</button>
+                <button className='bar_button'>Further Learning Units</button>
+                <button className='bar_button'>Whatever</button>
             </div>
-            <div>
-                <h2>Another Random Tutorial</h2>
+            <div className='tut'>
+                <div className='ching'>
+                    <h2>Random Tutorial</h2>
+                </div>
+                <div className='ching'>
+                    <h2>Another Random Tutorial</h2>
+                </div>
             </div>
         </div>
+        
     )
 }
 
--- src/Whatever.js
+++ src/Whatever.js
@@ -1,19 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './Whatever.css'
 
 function Whatever() {
+    const [tips,setTips] = useState(['there is no tip']);
+    const [sch,setSch] = useState(['the real schedule is inside the heart']);
+    const [tut,setTut] = useState(['BUET Tutorial']);
+    const [inp1, setInp1] = useState('');
+    const [inp2, setInp2] = useState('');
+    const [inp3, setInp3] = useState('');
+
+    var wee = () =>{
+        var a = inp1;
+        setTips([...tips,a]);
+        setInp1('');
+    }
+    var wee2 = () =>{
+        var a = inp2;
+        setSch([...sch,a]);
+        setInp2('');
+    }
+    var wee3 = () =>{
+        var a = inp3;
+        setTut([...tut,a]);
+        setInp3('');
+    }
     return (
-        <div>
-            <div>
+        <div className='whatever'>
+            <div className='what what1'>
                 <h2>Tips of the day</h2>
-                <p>The best tip is that there is no tip</p>
+                {tips.map(tip => <p>{tip}</p>)}
+                <input type="text" onChange = {(e) => setInp1(e.target.value)}  />
+                <button onClick={() =>wee()} >hyaa</button>
             </div>
-            <div>
+            <div className='what what2'>
                 <h2>Test Schedule</h2>
-                <p>A true man keeps his schedule in his heart</p>
+                {sch.map(sch => <p>{sch}</p>)}
+                <input type="text" onChange = {(e) => setInp2(e.target.value)}/>
+                <button onClick={() =>wee2()}>tou</button>
             </div>
-            <div>
+            <div className='what what3'>
                 <h2>My tutorials</h2>
-                <p>BUET Tutorial</p>
+                {tut.map(t => <p>{t}</p>)}
+                <input type="text"  onChange = {(e) => setInp3(e.target.value)}/>
+                <button onClick={() =>wee3()}>sore</button>
             </div>
         </div>
     )
